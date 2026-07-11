e32f56e26e308b524843e69afcf4036f99388d99
rupom516 <ramahalder135@gmail.com>
2026-07-11 16:37:05 +0530

    Add back to top button script

--- script.js
+++ script.js
@@ -20,3 +20,23 @@ function closeLightbox(){
 document.getElementById("lightbox").style.display="none";
 
 }
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function(){
+
+if(document.body.scrollTop>300 || document.documentElement.scrollTop>300){
+topBtn.style.display="block";
+}else{
+topBtn.style.display="none";
+}
+
+}
+
+function topFunction(){
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+}
